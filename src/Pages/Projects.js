fe050ad30302725b5eb1357416eb1036e7a3c3e8
Brian Hoang <bhoang35@gmail.com>
2018-10-06 21:13:03 +0100

    Added some skills and 1 project.

--- src/Pages/Projects.js
+++ src/Pages/Projects.js
@@ -1,38 +1,68 @@
-import React from 'react';
-import Project from '../Components/Card';
-import './Projects.css';
-import foodapp from '../Images/Projects/foodapp.png';
-import facerecog from '../Images/Projects/facerecog.png';
-import gem from '../Images/Projects/gem.png';
+import React from "react";
+import Project from "../Components/Card";
+import "./Projects.css";
+import foodapp from "../Images/Projects/foodapp.png";
+import facerecog from "../Images/Projects/facerecog.png";
+import gem from "../Images/Projects/gem.png";
+import dogapp from "../Images/Projects/dogapp.png";
 
-import {Animated} from "react-animated-css";
+import { Animated } from "react-animated-css";
 
 const foodApp = {
-	title: 'FoodApp',
-	'text': 'A progressive web app built in React where users can find places to eat using their current location or one they submit themselves.'
-}
+  title: "FoodApp",
+  text:
+    "A progressive web app built in React where users can find places to eat using their current location or one they submit themselves."
+};
 
 const faceRecog = {
-	title: 'Face Recognition',
-	'text': 'A website using a face recognition API to detect faces and identify the demographics inside. Has a fully functioning log-in system with hashed passwords.'
-}
+  title: "Face Recognition",
+  text:
+    "A website using a face recognition API to detect faces and identify the demographics inside. Has a fully functioning log-in system with hashed passwords."
+};
 
 const gemCalculator = {
-	title: 'Rainbow Gem Calculator',
-	'text': 'Tool for projecting currency for an online game, performing several calculations based on user inputs.'
-}
+  title: "Rainbow Gem Calculator",
+  text:
+    "Tool for projecting currency for an online game, performing several calculations based on user inputs."
+};
+
+const picturesOfDogs = {
+  title: "Pictures of Dogs",
+  text:
+    "Website which fetches pictures of dogs from an API, displaying them in modals as well as feauturing infinite scrolling and a robust navigation system."
+};
 
 const Projects = () => {
-	return(
-		<Animated animationIn="zoomInDown" animationOut="fadeOut" isVisible={true}>
-		<div id="projectGrid">
-			<Project cardTitle={foodApp.title} cardText={foodApp.text} cardImg={foodapp} buttonLink="https://bhoang93.github.io/foodapp/"/>
-			<Project cardTitle={faceRecog.title} cardText={faceRecog.text} cardImg={facerecog} buttonLink="http://facial-recog.herokuapp.com/"/>
-			<Project cardTitle={gemCalculator.title} cardText={gemCalculator.text} cardImg={gem} buttonLink="https://bhoang93.github.io/gemcalculator/"/>
-
-		</div>
-		</Animated>
-		)
-}
+  return (
+    <Animated animationIn="zoomInDown" animationOut="fadeOut" isVisible={true}>
+      <div id="projectGrid">
+        <Project
+          cardTitle={foodApp.title}
+          cardText={foodApp.text}
+          cardImg={foodapp}
+          buttonLink="https://bhoang93.github.io/foodapp/"
+        />
+        <Project
+          cardTitle={faceRecog.title}
+          cardText={faceRecog.text}
+          cardImg={facerecog}
+          buttonLink="http://facial-recog.herokuapp.com/"
+        />
+        <Project
+          cardTitle={gemCalculator.title}
+          cardText={gemCalculator.text}
+          cardImg={gem}
+          buttonLink="https://bhoang93.github.io/gemcalculator/"
+        />
+        <Project
+          cardTitle={picturesOfDogs.title}
+          cardText={picturesOfDogs.text}
+          cardImg={dogapp}
+          buttonLink="https://bhoang93.github.io/dog-app/"
+        />
+      </div>
+    </Animated>
+  );
+};
 
 export default Projects;
--- src/Pages/Skills.js
+++ src/Pages/Skills.js
@@ -1,236 +1,190 @@
-import React from 'react';
-import { ListGroup, ListGroupItem } from 'reactstrap';
-import {Animated} from "react-animated-css";
-import './Skills.css'
+import React from "react";
+import { ListGroup, ListGroupItem } from "reactstrap";
+import { Animated } from "react-animated-css";
+import "./Skills.css";
 
-import 'react-tippy/dist/tippy.css'
-import {
-  Tooltip,
-} from 'react-tippy';
+import "react-tippy/dist/tippy.css";
+import { Tooltip } from "react-tippy";
 
 //React Images
-import redux from '../Images/Logos/redux.png';
-import router from '../Images/Logos/router.png';
-import gatsby from '../Images/Logos/gatsby.png';
+import redux from "../Images/Logos/redux.png";
+import router from "../Images/Logos/router.png";
+import gatsby from "../Images/Logos/gatsby.png";
 
 //Front-End Images
-import html5 from '../Images/Logos/html5.png';
-import css3 from '../Images/Logos/css3.png';
-import bootstrap from '../Images/Logos/bootstrap.png';
+import html5 from "../Images/Logos/html5.png";
+import css3 from "../Images/Logos/css3.png";
+import bootstrap from "../Images/Logos/bootstrap.png";
 //JS Images
-import javascript from '../Images/Logos/javascript.png';
-import es6 from '../Images/Logos/es6.png'
-import nodejs from '../Images/Logos/nodejs.png';
-import reactLogo from '../Images/Logos/react.png';
-import jest from '../Images/Logos/jest.png';
-import typescript from '../Images/Logos/typescript.png';
+import javascript from "../Images/Logos/javascript.png";
+import es6 from "../Images/Logos/es6.png";
+import jquery from "../Images/Logos/jquery.png";
+import nodejs from "../Images/Logos/nodejs.png";
+import reactLogo from "../Images/Logos/react.png";
+import jest from "../Images/Logos/jest.png";
+import typescript from "../Images/Logos/typescript.png";
 //Back-End Images
-import express from '../Images/Logos/express.png';
-import postgresql from '../Images/Logos/postgresql.png';
-import redis from '../Images/Logos/redis.png';
-import graphql from '../Images/Logos/graphql.png';
+import php from "../Images/Logos/php.png";
+import express from "../Images/Logos/express.png";
+import postgresql from "../Images/Logos/postgresql.png";
+import redis from "../Images/Logos/redis.png";
+import graphql from "../Images/Logos/graphql.png";
 //Other Images
-import github from '../Images/Logos/github.png';
-import heroku from '../Images/Logos/heroku.png';
-import docker from '../Images/Logos/docker.png';
-import aws from '../Images/Logos/aws.png';
-import photoshop from '../Images/Logos/photoshop.png';
-import premiere from '../Images/Logos/premiere.png';
-import circleci from '../Images/Logos/circleci.png';
+import github from "../Images/Logos/github.png";
+import heroku from "../Images/Logos/heroku.png";
+import docker from "../Images/Logos/docker.png";
+import aws from "../Images/Logos/aws.png";
+import photoshop from "../Images/Logos/photoshop.png";
+import premiere from "../Images/Logos/premiere.png";
+import circleci from "../Images/Logos/circleci.png";
 
 const Skills = () => {
-	return(
-		<Animated animationIn="zoomInDown" animationOut="fadeOut" isVisible={true}>
- 		<ListGroup className="skills">
+  return (
+    <Animated animationIn="zoomInDown" animationOut="fadeOut" isVisible={true}>
+      <ListGroup className="skills">
+        <ListGroupItem className="skillRow">
+          <h5>React</h5>
+          <div className="skillLogos">
+            <Tooltip title="Redux" position="top" trigger="mouseenter focus">
+              <img className="logos" src={redux} />
+            </Tooltip>
+            <Tooltip
+              title="React Router"
+              position="top"
+              trigger="mouseenter focus"
+            >
+              <img className="logos" src={router} />
+            </Tooltip>
+            <Tooltip title="Gatsby" position="top" trigger="mouseenter focus">
+              <img className="logos" src={gatsby} />
+            </Tooltip>
+          </div>
+        </ListGroupItem>
 
- 		<ListGroupItem className="skillRow">
-	        	<h5>React</h5>
-	        		<div className="skillLogos">
-	        		<Tooltip
-						  title="Redux"
-						  position="top"
-						  trigger="mouseenter focus"
-						>
-	        			<img className="logos" src={redux} />
-	        			</Tooltip>
-	        		<Tooltip
-						  title="React Router"
-						  position="top"
-						  trigger="mouseenter focus"
-						>	        		
-	        			<img className="logos" src={router} />
-	        		</Tooltip>
-	        		<Tooltip
-						  title="Gatsby"
-						  position="top"
-						  trigger="mouseenter focus"
-						>
-	        			<img className="logos" src={gatsby} />
-	        		</Tooltip>
-	        		</div>
-	        </ListGroupItem>
+        <ListGroupItem className="skillRow">
+          <h5>Front-End</h5>
+          <div className="skillLogos">
+            <Tooltip title="HTML5" position="top" trigger="mouseenter focus">
+              <img className="logos" src={html5} />
+            </Tooltip>
+            <Tooltip title="CSS3" position="top" trigger="mouseenter focus">
+              <img className="logos" src={css3} />
+            </Tooltip>
+            <Tooltip
+              title="Bootstrap"
+              position="top"
+              trigger="mouseenter focus"
+            >
+              <img className="logos" src={bootstrap} />
+            </Tooltip>
+          </div>
+        </ListGroupItem>
 
-	        <ListGroupItem className="skillRow">
-	        	<h5>Front-End</h5>
-	        		<div className="skillLogos">
-	        		<Tooltip
-						  title="HTML5"
-						  position="top"
-						  trigger="mouseenter focus"
-						>
-	        			<img className="logos" src={html5} />
-	        			</Tooltip>
-	        		<Tooltip
-						  title="CSS3"
-						  position="top"
-						  trigger="mouseenter focus"
-						>	        		
-	        			<img className="logos" src={css3} />
-	        		</Tooltip>
-	        		<Tooltip
-						  title="Bootstrap"
-						  position="top"
-						  trigger="mouseenter focus"
-						>
-	        			<img className="logos" src={bootstrap} />
-	        		</Tooltip>
-	        		</div>
-	        </ListGroupItem>
+        <ListGroupItem className="skillRow">
+          <h5>JavaScript</h5>
+          <div className="skillLogos">
+            <Tooltip
+              title="JavaScript"
+              position="top"
+              trigger="mouseenter focus"
+            >
+              <img className="logos" src={javascript} />
+            </Tooltip>
 
-	        <ListGroupItem className="skillRow">
-	        	<h5>JavaScript</h5>
-	        		<div className="skillLogos">
-	        			<Tooltip
-						  title="JavaScript"
-						  position="top"
-						  trigger="mouseenter focus"
-						>
-	        			<img className="logos" src={javascript} />
-	        			</Tooltip>
-						<Tooltip
-						  title="NodeJS"
-						  position="top"
-						  trigger="mouseenter focus"
-						>
-						<img className="logos" src={nodejs} />
-						</Tooltip>
+            <Tooltip title="jQuery" position="top" trigger="mouseenter focus">
+              <img className="logos" src={jquery} />
+            </Tooltip>
 
-						<Tooltip
-						  title="Jest"
-						  position="top"
-						  trigger="mouseenter focus"
-						>
-	        			<img className="logos" src={jest} />
-	        			</Tooltip>
+            <Tooltip title="NodeJS" position="top" trigger="mouseenter focus">
+              <img className="logos" src={nodejs} />
+            </Tooltip>
 
-						<Tooltip
-						  title="TypeScript"
-						  position="top"
-						  trigger="mouseenter focus"
-						>
-	        			<img className="logos" src={typescript} />
-	        			</Tooltip>
+            <Tooltip title="Jest" position="top" trigger="mouseenter focus">
+              <img className="logos" src={jest} />
+            </Tooltip>
 
-	        		</div>
-	        </ListGroupItem>
+            <Tooltip
+              title="TypeScript"
+              position="top"
+              trigger="mouseenter focus"
+            >
+              <img className="logos" src={typescript} />
+            </Tooltip>
+          </div>
+        </ListGroupItem>
 
-	        <ListGroupItem className="skillRow">
-	        	<h5>Back-End</h5>
-	        		<div className="skillLogos">
-	        		<Tooltip
-						  title="ExpressJS"
-						  position="top"
-						  trigger="mouseenter focus"
-						>
-	        			<img className="logos" src={express} />
-	        		</Tooltip>
-	        		<Tooltip
-						  title="PostgreSQLl"
-						  position="top"
-						  trigger="mouseenter focus"
-						>
-	        			<img className="logos" src={postgresql} />
-	        		</Tooltip>
-	        		<Tooltip
-						  title="Redis"
-						  position="top"
-						  trigger="mouseenter focus"
-						>
-	        			<img className="logos" src={redis} />
-	        		</Tooltip>
-	        		<Tooltip
-						  title="GraphQL"
-						  position="top"
-						  trigger="mouseenter focus"
-						>
-	        			<img className="logos" src={graphql} />
-	        		</Tooltip>
-	        		</div>
-	        </ListGroupItem>
+        <ListGroupItem className="skillRow">
+          <h5>Back-End</h5>
+          <div className="skillLogos">
+            <Tooltip title="PHP" position="top" trigger="mouseenter focus">
+              <img className="logos" src={php} />
+            </Tooltip>
 
-	        <ListGroupItem className="skillRow">
-	        	<h5>Other</h5>
-	        		<div className="skillLogos">
-	        	<Tooltip
-						  title="Github"
-						  position="top"
-						  trigger="mouseenter focus"
-						>
-	        			<img className="logos" src={github} />
-	        	</Tooltip>
-	        	<Tooltip
-						  title="Heroku"
-						  position="top"
-						  trigger="mouseenter focus"
-						>
-	        			<img className="logos" src={heroku} />
-	        	</Tooltip>
+            <Tooltip
+              title="ExpressJS"
+              position="top"
+              trigger="mouseenter focus"
+            >
+              <img className="logos" src={express} />
+            </Tooltip>
+            <Tooltip
+              title="PostgreSQLl"
+              position="top"
+              trigger="mouseenter focus"
+            >
+              <img className="logos" src={postgresql} />
+            </Tooltip>
+            <Tooltip title="Redis" position="top" trigger="mouseenter focus">
+              <img className="logos" src={redis} />
+            </Tooltip>
+            <Tooltip title="GraphQL" position="top" trigger="mouseenter focus">
+              <img className="logos" src={graphql} />
+            </Tooltip>
+          </div>
+        </ListGroupItem>
 
-	        	<Tooltip
-						  title="Docker"
-						  position="top"
-						  trigger="mouseenter focus"
-						>
-	        			<img className="logos" src={docker} />
-	        	</Tooltip>
+        <ListGroupItem className="skillRow">
+          <h5>Other</h5>
+          <div className="skillLogos">
+            <Tooltip title="Github" position="top" trigger="mouseenter focus">
+              <img className="logos" src={github} />
+            </Tooltip>
+            <Tooltip title="Heroku" position="top" trigger="mouseenter focus">
+              <img className="logos" src={heroku} />
+            </Tooltip>
 
-	        	<Tooltip
-						  title="Amazon Web Services"
-						  position="top"
-						  trigger="mouseenter focus"
-						>
-	        			<img className="logos" src={aws} />
-	        	</Tooltip>
+            <Tooltip title="Docker" position="top" trigger="mouseenter focus">
+              <img className="logos" src={docker} />
+            </Tooltip>
 
-	      	    <Tooltip
-						  title="CircleCI"
-						  position="top"
-						  trigger="mouseenter focus"
-						>
-	        			<img className="logos" src={circleci} />
-	        	</Tooltip>
+            <Tooltip
+              title="Amazon Web Services"
+              position="top"
+              trigger="mouseenter focus"
+            >
+              <img className="logos" src={aws} />
+            </Tooltip>
 
-	        	<Tooltip
-						  title="Photoshop"
-						  position="top"
-						  trigger="mouseenter focus"
-						>
-	        			<img className="logos" src={photoshop} />
-	        	</Tooltip>
+            <Tooltip title="CircleCI" position="top" trigger="mouseenter focus">
+              <img className="logos" src={circleci} />
+            </Tooltip>
 
-	        	<Tooltip
-						  title="Premiere"
-						  position="top"
-						  trigger="mouseenter focus"
-						>
-	        			<img className="logos" src={premiere} />
-	        	</Tooltip>
+            <Tooltip
+              title="Photoshop"
+              position="top"
+              trigger="mouseenter focus"
+            >
+              <img className="logos" src={photoshop} />
+            </Tooltip>
 
-	        		</div>
-	        </ListGroupItem>
+            <Tooltip title="Premiere" position="top" trigger="mouseenter focus">
+              <img className="logos" src={premiere} />
+            </Tooltip>
+          </div>
+        </ListGroupItem>
       </ListGroup>
-		</Animated>
-		)
-}
+    </Animated>
+  );
+};
 
 export default Skills;
